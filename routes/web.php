8167ec427bc4c03e4237519662c067cbe53d6590
Muhammad Ribhi Ramadan <ribhiramadhan11@gmail.com>
2025-12-01 15:02:17 +0700

    Update homepage UI + Vite integration

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/tes', function () {
-    return view('tes');
+Route::get('/', function () {
+    return view('home');
 });
